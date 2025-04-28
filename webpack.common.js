d50d9ac4b1ab42767970857df6ab2b474df2b96e
Anton Pakhomov <antonn.pakhomovv@gmail.com>
2025-04-28 16:12:25 +0400

    fix webpack

--- webpack.common.js
+++ webpack.common.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
   target: 'web',
@@ -11,12 +10,6 @@ module.exports = {
   },
   module: {
     rules: [
-      {
-        test: /web-worker\.js$/,
-        use: {
-          loader: 'worker-loader',
-        },
-      },
       {
         test: /\.js$/,
         exclude: /node_modules/,
@@ -50,10 +43,6 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: '[name].css',
       chunkFilename: '[id].css',
-    }),
-    new WorkboxPlugin.InjectManifest({
-      // swSrc: './src/js/service.worker.js',
-      // swDest: 'service-worker.js',
     }),
   ],
 };
