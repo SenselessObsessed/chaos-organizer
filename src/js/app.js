2b9e18e149591a15ebb442b99bcb622dbcc2209c
Anton Pakhomov <antonn.pakhomovv@gmail.com>
2025-05-03 00:20:47 +0400

    add tasks

--- src/js/app.js
+++ src/js/app.js
@@ -3,23 +3,3 @@ import "./searchLink";
 
 const chaosOrganizer = new ChaosOrganizer(document.body);
 chaosOrganizer.bindToDOM();
-
-// document
-//   .querySelector(".messages__container")
-//   .addEventListener("dragenter", () => {
-//     const placeKaroch = document.querySelector(".drop");
-//     placeKaroch.style.display = "block";
-//   });
-
-// document.querySelector(".messages").addEventListener("dragenter", () => {
-//   const placeKaroch = document.querySelector(".drop");
-//   placeKaroch.style.display = "block";
-// });
-
-// document.querySelector(".drop").addEventListener("dragleave", (e) => {
-//   e.target.style.display = "none";
-// });
-
-// document.querySelector(".drop-file").addEventListener("drop", () => {
-//   document.querySelector(".drop").style.display = "none";
-// });
